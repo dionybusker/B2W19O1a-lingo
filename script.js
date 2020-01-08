0e071fcfc930f28fecf3b4819feed0a26dc81aa9
dionybusker <99057959@mydavinci.nl>
2020-01-08 10:16:46 +0100

    lay out lingo

---
+++ script.js
@@ -0,0 +1,13 @@
+for (i = 1; i <= 25; i++) {
+    var p = document.createElement("p");
+        p.innerText = i;
+        p.style.border = "1px solid black";
+        p.style.width = "50px";
+        p.style.height = "50px";
+        p.style.margin = "10px";
+        p.style.fontSize = "50px";
+        p.style.backgroundColor = "grey";
+
+        var gridContainer = document.getElementById("grid-container");
+            gridContainer.appendChild(p);
+}
