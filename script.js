58168c9b936cd5800f942db24e8550e029260197
dionybusker <99057959@mydavinci.nl>
2020-01-08 12:16:11 +0100

    extra for loop om rijen te maken (nu wordt alles 5x weergegeven)

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 var gameContainer = document.getElementById("game");
 
-for (i = 1; i <= 25; i++) {
-    var p = document.createElement("p");
-        // p.innerText = i;
-        p.style.border = "1px solid black";
-        p.style.width = "75px";
-        p.style.height = "50px";
-        p.style.margin = "10px";
-        p.style.fontSize = "50px";
-        p.style.display = "inline-block";
-        p.style.textAlign = "center";
+for (rows = 1; rows <= 5; rows++) {
+    var div = document.createElement("div");
+        gameContainer.appendChild(div);
 
-        gameContainer.appendChild(p);
+    for (i = 1; i <= 25; i++) {
+        var p = document.createElement("p");
+            p.innerText = i;
+            p.style.border = "1px solid black";
+            p.style.width = "75px";
+            p.style.height = "50px";
+            p.style.margin = "10px";
+            p.style.fontSize = "50px";
+            p.style.display = "inline-block";
+            p.style.textAlign = "center";
+
+            p.setAttribute("class", "default");
+
+            gameContainer.appendChild(p);
+    }
+
+    div.setAttribute("class", `row${rows}`)
+
 }
+
 
