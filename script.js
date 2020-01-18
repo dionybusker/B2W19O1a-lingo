791bb247907f538e2cc7304280f389134c423b4e
dionybusker <99057959@mydavinci.nl>
2020-01-18 19:39:03 +0100

    layout in js (opnieuw) gemaakt
    button, input, h1, p etc toegevoegd. Dit stond eerst allemaal in de html, deels styling weggehaald en in de css gezet, code die niet (meer) gebruikt gaat worden verwijderd

--- script.js
+++ script.js
@@ -1,36 +1,54 @@
-var gameContainer = document.getElementById("game");
+var body = document.getElementsByTagName("body")[0];
+
+var gameContainer = document.createElement("div");
+    gameContainer.setAttribute("id", "game-container");
     gameContainer.style.width = "485px";
+    body.appendChild(gameContainer);
+
+var h1 = document.createElement("h1");
+    h1.innerText = "Lingo";
+    gameContainer.appendChild(h1);
+
+var p = document.createElement("p");
+    p.innerText = "Groene vierkant: letter staat op de juiste plek. \n Gele cirkel: letter zit in het woord maar staat op de verkeerde plek.";
+    gameContainer.appendChild(p);
+
+var gameDiv = document.createElement("div");
+    gameDiv.setAttribute("id", "game");
+    gameContainer.appendChild(gameDiv);
+
+var input = document.createElement("input");
+    input.setAttribute("id", "input");
+    input.setAttribute("type", "text");
+    input.setAttribute("placeholder", "typ hier je antwoord");
+    gameContainer.appendChild(input);
+
+var button = document.createElement("button");
+    button.innerText = "spelen";
+    button.setAttribute("onclick", "check()");
+    gameContainer.appendChild(button);
+
 
 for (rows = 0; rows < 5; rows++) {
     var div = document.createElement("div");
-        gameContainer.appendChild(div);
-        div.setAttribute("class", `row_${rows+1}`);
+        gameDiv.appendChild(div);
+        div.setAttribute("class", "rows");
+        div.setAttribute("id", `row_${rows+1}`)
 
     for (i = 0; i < 5; i++) {
-        var p = document.createElement("p");
-            // p.innerText = i + 1;
-            p.style.border = "1px solid black";
-            p.style.width = "75px";
-            p.style.height = "75px";
-            p.style.margin = "10px";
-            p.style.fontSize = "50px";
-            p.style.display = "inline-block";
-            p.style.textAlign = "center";
-
-            p.setAttribute("class", "default");
-            p.setAttribute("id", `letter_${i + 1}`)
+        var letterBox = document.createElement("p");
+            letterBox.setAttribute("class", "default");
+            letterBox.setAttribute("id", `letter_${i + 1}`);
             
-            div.appendChild(p);
+            div.appendChild(letterBox);
     }
 }
 
-// var input = document.getElementById("input");
-var button = document.getElementById("button");
-    button.setAttribute("onclick", "check()");
 
 var randomWord = "kaart";
 // var randomWord = words[Math.floor(Math.random() * words.length)];
     console.log(randomWord);
+var beurt = 0;
 
 // var word = document.getElementsByClassName(`row_${i}`).value;
 
@@ -42,17 +60,20 @@ var randomWord = "kaart";
 
 var firstLetter = document.getElementById(`letter_${1}`);
     firstLetter.innerText = randomWord[0];
-    // firstLetter.toUpperCase();
+
+var wordArray = randomWord.split("");
 
 function check() {
-
     var guess = document.getElementById("input").value;
-
-    var wordArray = randomWord.split("");
+    var wordArrayCopy = wordArray;
     var guessArray = guess.split("");
     
+    // guessArray.forEach(function(value, index) {
+    //     document.getElementById()
+    // })
+
     for (var i = 0; i < guessArray.length; i++) {
-        if (guessArray[i] == wordArray[i]) {
+        if (guessArray[i] == wordArrayCopy[i]) {
             var position = document.getElementById(`letter_${i + 1}`);
                 position.style.backgroundColor = "green";
                 position.innerText = guessArray[i];
@@ -64,21 +85,20 @@ function check() {
 
     for (var i = 0; i < guessArray.length; i++) {
         if (guessArray[i] != null) {
-            if (wordArray.indexOf(guessArray[i]) != -1) {
+            if (wordArrayCopy.indexOf(guessArray[i]) != -1) {
                 var position = document.getElementById(`letter_${i + 1}`);
                     position.style.backgroundColor = "yellow";
                     position.innerText = guessArray[i];
 
-                var pos = wordArray.indexOf(guessArray[i]);
+                var pos = wordArrayCopy.indexOf(guessArray[i]);
                 guessArray[i] = null;
-                wordArray[pos] = null;
+                wordArrayCopy[pos] = null;
             }
         }
-        // var nextRow = document.getElementsByClassName(`row_${i}`);
-        //     nextRow = nextRow + 1;
     }
 
-    console.log(wordArray);
+    console.log(wordArrayCopy);
     console.log(guessArray);
+    beurt++;
 }
 
